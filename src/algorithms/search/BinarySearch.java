10ffd6a1cc6d98e98cfe6895a8643e0c260b7bd2
Satish Singh <sattish1996@yahoo.in>
2022-09-04 11:16:17 +0530

    commiting binary search

--- src/algorithms/search/BinarySearch.java
+++ src/algorithms/search/BinarySearch.java
@@ -1,10 +1,34 @@
 package algorithms.search;
 
 public class BinarySearch {
+	 public static int search(int[] nums, int target) {
+	        int h = nums.length -1;
+	        int l = 0;
+	        int mid = 0;
+	        while(h>=l)
+	        {
+	        	//count++;
+	            mid = l + (h -l)/2;
+	            if(nums[mid]==target)
+	            {
+	                return mid;
+	            }
+	            else if (target > nums[mid])
+	            {
+	                l = mid + 1;
+	            }
+	            else
+	            {
+	                h = mid - 1;
+	            }
+	            
+	        }
+	        return -1;
+	    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(BinarySearch.search(new int[] {1,2,3,45},3));
 	}
 
 }
+
